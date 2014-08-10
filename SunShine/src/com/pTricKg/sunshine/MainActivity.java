5b172a5527d933374b631ce359905314b49940c7
pTricKg <ptrickg1976@gmail.com>
2014-08-10 16:00:16 -0600

    moved fragment layout to main, commented out error in activity, and changed fragment main to frame layout

--- SunShine/src/com/pTricKg/sunshine/MainActivity.java
+++ SunShine/src/com/pTricKg/sunshine/MainActivity.java
@@ -17,10 +17,10 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+//		if (savedInstanceState == null) {
+//			getSupportFragmentManager().beginTransaction()
+//					.add(R.id.container, new PlaceholderFragment()).commit();
+//		}
 	}
 
 	@Override
