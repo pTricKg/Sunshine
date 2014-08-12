95c653e6869316c8690abaa5fbe04f3f69b28aef
pTricKg <ptrickg1976@gmail.com>
2014-08-11 22:42:34 -0600

    added settings activity

--- SunShine/gen/com/pTricKg/sunshine/R.java
+++ SunShine/gen/com/pTricKg/sunshine/R.java
@@ -1199,15 +1199,18 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_detail=0x7f030018;
         public static final int activity_main=0x7f030019;
-        public static final int fragment_detail=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int list_item_forecast=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int activity_settings=0x7f03001a;
+        public static final int fragment_detail=0x7f03001b;
+        public static final int fragment_main=0x7f03001c;
+        public static final int fragment_settings=0x7f03001d;
+        public static final int list_item_forecast=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
         public static final int detail=0x7f0c0000;
         public static final int forecastfragment=0x7f0c0001;
         public static final int main=0x7f0c0002;
+        public static final int settings=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1255,6 +1258,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_detail=0x7f0a0011;
+        public static final int title_activity_settings=0x7f0a0012;
     }
     public static final class style {
         /** 
