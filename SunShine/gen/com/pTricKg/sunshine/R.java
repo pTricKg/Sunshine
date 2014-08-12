8e2ab87448db26a5ca2122cd14daa3d1cdd0cd1a
pTricKg <ptrickg1976@gmail.com>
2014-08-11 21:17:18 -0600

    add detail activity

--- SunShine/gen/com/pTricKg/sunshine/R.java
+++ SunShine/gen/com/pTricKg/sunshine/R.java
@@ -1091,8 +1091,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_refresh=0x7f05003f;
-        public static final int action_settings=0x7f050040;
+        public static final int action_refresh=0x7f050040;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1196,14 +1196,17 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int list_item_forecast=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_detail=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int fragment_detail=0x7f03001a;
+        public static final int fragment_main=0x7f03001b;
+        public static final int list_item_forecast=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
-        public static final int forecastfragment=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int detail=0x7f0c0000;
+        public static final int forecastfragment=0x7f0c0001;
+        public static final int main=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1250,6 +1253,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_detail=0x7f0a0011;
     }
     public static final class style {
         /** 
