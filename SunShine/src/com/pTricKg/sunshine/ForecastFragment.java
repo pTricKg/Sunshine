8e3b947ef59a4f02f2b94418600fc06a03f6d2ec
pTricKg <ptrickg1976@gmail.com>
2014-08-12 18:43:23 -0600

    updated method to use sharedpref for high/low temps

--- SunShine/src/com/pTricKg/sunshine/ForecastFragment.java
+++ SunShine/src/com/pTricKg/sunshine/ForecastFragment.java
@@ -160,6 +160,21 @@ public class ForecastFragment extends Fragment {
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
+        	// data is fetched in metric by default.  If fahrenheit selected
+        	// conversion initiated below.  Data refreshed on the fly so
+        	// database doesn't have to be rewritten
+            SharedPreferences sharedPrefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(
+                    getString(R.string.pref_temp_units_key),
+                    getString(R.string.pref_temp_units_metric));
+
+            if (unitType.equals(getString(R.string.pref_temp_units_imperial))) {
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            } else if (!unitType.equals(getString(R.string.pref_temp_units_metric))) {
+                Log.d(LOG_TAG, "Unit type not found: " + unitType);
+            }
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
