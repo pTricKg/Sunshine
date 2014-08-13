94b1a761a5f45136c2ff0d09a013c768f6560233
pTricKg <ptrickg1976@gmail.com>
2014-08-13 17:18:58 -0600

    added uri matcher for content provider queries

--- SunShine/src/com/pTricKg/sunshine/data/WeatherProvider.java
+++ SunShine/src/com/pTricKg/sunshine/data/WeatherProvider.java
@@ -2,6 +2,7 @@ package com.pTricKg.sunshine.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -13,6 +14,28 @@ public class WeatherProvider extends ContentProvider {
 	private static final int WEATHER_WITH_LOCATION_AND_DATE = 102;
 	private static final int LOCATION = 300;
 	private static final int LOCATION_ID = 301;
+	
+	// content provider uri matcher
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+	private static UriMatcher buildUriMatcher() {
+		
+		// All paths added to the UriMatcher have a corresponding code to return when a match is
+        // found.  The code passed into the constructor represents the code to return for the root
+        // URI.  It's common to use NO_MATCH as the code for this case.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding code.
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/*", WEATHER_WITH_LOCATION_AND_DATE);
+
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION + "/#", LOCATION_ID);
+
+        return matcher;
+	}
 	
 	@Override
 	public boolean onCreate() {
