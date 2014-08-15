b0bfe29711726c4e5c377fabd8bb8e16a34bbc14
pTricKg <ptrickg1976@gmail.com>
2014-08-15 00:56:02 -0600

    add LOCATION switch case to insert() that i missed last time and added content observer to watch for changes

--- SunShine/src/com/pTricKg/sunshine/data/WeatherProvider.java
+++ SunShine/src/com/pTricKg/sunshine/data/WeatherProvider.java
@@ -240,19 +240,28 @@ public class WeatherProvider extends ContentProvider {
 
 		switch (match) {
 			
-			case WEATHER:
+			case WEATHER: {
 				long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, values);
                 if ( _id > 0 )
                     returnUri = WeatherContract.WeatherEntry.buildWeatherUri(_id);
                 else
                     throw new android.database.SQLException("Failed to insert row into " + uri);
                 break;
-			case LOCATION:
+			}
+			case LOCATION: {
+				long _id = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, values);
+                if ( _id > 0 )
+                    returnUri = WeatherContract.LocationEntry.buildLocationUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
 				break;
+			}
 			default:
 				throw new UnsupportedOperationException("Unknown uri: " + uri);
 				}
-				return returnUri;
+		// notify observers on changes made
+		getContext().getContentResolver().notifyChange(uri, null);
+		return returnUri;
 	}
 	
 	@Override
