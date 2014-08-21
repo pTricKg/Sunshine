eaba89f0b4be6816e4af1ed3c76d1de42e15bb7e
pTricKg <ptrickg1976@gmail.com>
2014-08-20 20:03:34 -0600

    added getDateFromDb method

--- SunShine/src/com/pTricKg/sunshine/data/WeatherContract.java
+++ SunShine/src/com/pTricKg/sunshine/data/WeatherContract.java
@@ -1,5 +1,6 @@
 package com.pTricKg.sunshine.data;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -36,6 +37,21 @@ public class WeatherContract {
         // it must be converted to milliseconds in order to be converted to valid date.
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
         return sdf.format(date);
+    }
+    
+    /**
+     * Converts a dateText to a long Unix time representation
+     * @param dateText the input date string
+     * @return the Date object
+     */
+    public static Date getDateFromDb(String dateText) {
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
+        try {
+            return dbDateFormat.parse(dateText);
+        } catch ( ParseException e ) {
+            e.printStackTrace();
+            return null;
+        }
     }
 			
     /* Inner class that defines the table contents of the weather table */
