88261bacaef25f3a9db095c7d8a385051f6c2419
pTricKg <ptrickg1976@gmail.com>
2014-08-20 21:27:08 -0600

    typo fix

--- SunShine/src/com/pTricKg/sunshine/ForecastFragment.java
+++ SunShine/src/com/pTricKg/sunshine/ForecastFragment.java
@@ -3,7 +3,7 @@ package com.pTricKg.sunshine;
 import java.util.Date;
 
 import android.content.Intent;
-import android.database.Cursor;Untitled - OneDrive
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
