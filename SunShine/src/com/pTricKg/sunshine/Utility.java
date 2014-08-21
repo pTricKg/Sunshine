4c4f2c4f4be5f1fe31f0c033fcf127870ce4bb7a
pTricKg <ptrickg1976@gmail.com>
2014-08-20 20:36:56 -0600

    fixed up utility methods / back to working pulling data: detail needs work still

--- SunShine/src/com/pTricKg/sunshine/Utility.java
+++ SunShine/src/com/pTricKg/sunshine/Utility.java
@@ -1,5 +1,10 @@
 package com.pTricKg.sunshine;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import com.pTricKg.sunshine.data.WeatherContract;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -12,19 +17,25 @@ public class Utility {
                 context.getString(R.string.pref_location_default));
     }
 
-	public static CharSequence formatTemperature(double double1,
-			boolean isMetric) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public static boolean isMetric(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(context.getString(R.string.pref_temp_units_key),
+                context.getString(R.string.pref_temp_units_metric))
+                .equals(context.getString(R.string.pref_temp_units_metric));
+    }
 
-	public static CharSequence formatDate(String dateString) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    static String formatTemperature(double temperature, boolean isMetric) {
+        double temp;
+        if ( !isMetric ) {
+            temp = 9*temperature/5+32;
+        } else {
+            temp = temperature;
+        }
+        return String.format("%.0f", temp);
+    }
 
-	public static boolean isMetric(FragmentActivity activity) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    static String formatDate(String dateString) {
+        Date date = WeatherContract.getDateFromDb(dateString);
+        return DateFormat.getDateInstance().format(date);
+    }
 }
