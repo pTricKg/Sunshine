ed0038c5fb0b703d6e6314cd57ede156757fbab4
pTricKg <ptrickg1976@gmail.com>
2014-08-24 19:37:15 -0600

    added fixings for viewType config in first layout

--- SunShine/src/com/pTricKg/sunshine/ForecastAdapter.java
+++ SunShine/src/com/pTricKg/sunshine/ForecastAdapter.java
@@ -22,18 +22,30 @@ public class ForecastAdapter extends CursorAdapter {
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
-    
+    @Override
     public int getItemViewType(int position) {
     	return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
     }
-    
+    @Override
     public int getViewTypeCount() {
     	return 2;
     }
  
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+    	int viewType = getItemViewType(cursor.getPosition());
+    	int layoutId = -1;
+    	switch (viewType) {
+        case VIEW_TYPE_TODAY: {
+            layoutId = R.layout.list_item_forecast_today;
+            break;
+        }
+        case VIEW_TYPE_FUTURE_DAY: {
+            layoutId = R.layout.list_item_forecast;
+            break;
+        }
+    	}
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
  
     @Override
